38ed955f7a9655dfb1c9ca5709a1b726e5e1be2c
UserUnknownFactor <63057995+UserUnknownFactor@users.noreply.github.com>
2020-11-12 17:32:18 +0300

    Move files to the root

--- kside/js/autorefresh.js
+++
@@ -1,7 +0,0 @@
-"use strict";
-function checkModifications() {
-    $.getJSON("/onchange", () => location.reload(true)).fail(() => setTimeout(checkModifications, 750));
-}
-if (0 && location.hostname === "127.0.0.1")
-    checkModifications();
-//# sourceMappingURL=autorefresh.js.map
